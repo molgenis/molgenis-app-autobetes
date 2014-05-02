c0c5818b90c5a5c98dcc1dbd24e61d76b321aaf4
erwinwinder <j.a.winder@umcg.nl>
2014-05-02 16:20:42 +0200

    Make use of JpaStandaloneDataService

--- src/main/java/org/molgenis/autobetes/ImportCommandLine.java
+++ src/main/java/org/molgenis/autobetes/ImportCommandLine.java
@@ -1,19 +1,8 @@
 package org.molgenis.autobetes;
 
-import java.util.Map;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
-
 import org.molgenis.autobetes.autobetes.Event;
-import org.molgenis.autobetes.autobetes.EventRepository;
 import org.molgenis.data.DataService;
-import org.molgenis.data.support.DataServiceImpl;
-import org.molgenis.data.support.QueryResolver;
-import org.molgenis.data.validation.DefaultEntityValidator;
-import org.molgenis.data.validation.EntityAttributesValidator;
-
-import com.google.common.collect.Maps;
+import org.molgenis.data.jpa.standalone.JpaStandaloneDataService;
 
 public class ImportCommandLine
 {
@@ -24,40 +13,26 @@ public class ImportCommandLine
 
 	public void importExample()
 	{
-		DataService dataService = new DataServiceImpl();
 
-		Map<String, String> props = Maps.newHashMap();
-		props.put("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
-		props.put("javax.persistence.jdbc.url",
-				"jdbc:mysql://localhost/autobetes?innodb_autoinc_lock_mode=2&amp;rewriteBatchedStatements=true");
-		props.put("javax.persistence.jdbc.user", "molgenis");
-		props.put("javax.persistence.jdbc.password", "molgenis");
-
-		EntityManager entityManager = Persistence.createEntityManagerFactory("molgenis", props).createEntityManager();
-
-		EventRepository eventRepo = new EventRepository(entityManager, new DefaultEntityValidator(dataService,
-				new EntityAttributesValidator()), new QueryResolver(dataService));
-		dataService.addRepository(eventRepo);
-
-		entityManager.getTransaction().begin();
 		try
 		{
-			Event event = new Event();
-			event.setEventType("food");
-			event.setName("Apple");
-			dataService.add(Event.ENTITY_NAME, event);
+			DataService dataService = new JpaStandaloneDataService(
+					"jdbc:mysql://localhost/autobetes?innodb_autoinc_lock_mode=2&amp;rewriteBatchedStatements=true",
+					"molgenis", "molgenis");
 
-			entityManager.getTransaction().commit();
-			System.out.println("Event added");
+			Event pearEvent = new Event();
+			pearEvent.setEventType("food");
+			pearEvent.setName("Pear");
+			dataService.add(Event.ENTITY_NAME, pearEvent);
+
+			for (Event event : dataService.findAll(Event.ENTITY_NAME, Event.class))
+			{
+				System.out.println(event);
+			}
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
-			entityManager.getTransaction().rollback();
-		}
-		finally
-		{
-			entityManager.close();
 		}
 
 	}
