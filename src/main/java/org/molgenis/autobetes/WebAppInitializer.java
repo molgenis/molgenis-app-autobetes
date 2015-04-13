d564b89381c4a1ca074eb123a1b8f04a0d3efb14
mdijkstra <mdijkstrasEmailAddressIsHidden>
2015-04-13 14:29:27 +0200

    Use molgenis 1.4.3 (stable). Fixed 'cross origin' access problem, so users can register.

--- src/main/java/org/molgenis/autobetes/WebAppInitializer.java
+++ src/main/java/org/molgenis/autobetes/WebAppInitializer.java
@@ -1,8 +1,14 @@
 package org.molgenis.autobetes;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.FilterRegistration.Dynamic;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.molgenis.security.CorsFilter;
 import org.molgenis.ui.MolgenisWebAppInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
@@ -12,5 +18,7 @@ public class WebAppInitializer extends MolgenisWebAppInitializer implements WebA
 	public void onStartup(ServletContext servletContext) throws ServletException
 	{
 		super.onStartup(servletContext, WebAppConfig.class, true);
+		FilterRegistration corsFilter = servletContext.getFilterRegistration("corsFilter");
+		corsFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/plugin/anonymous/*");
 	}
 }
